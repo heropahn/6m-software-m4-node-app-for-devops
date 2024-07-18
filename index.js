eb253a00841a194588cd3cd5b497017cd80779f7
Hyeon (George) <heropahn@icloud.com>
2024-07-18 22:20:32 +0800

    retry

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+app.use(express.json());
 
 const print = require("./controller");
 
-app.get('/', print);
+app.get("/", print);
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Listening to port ${process.env.PORT}`)
-})
+app.get("/home", (req, res) => {
+  res.send("This is the home page");
+});
+
+app.post("/user", (req, res) => {
+  const { name, email, password } = req.body;
+  res.send(`Name: ${name}, Email: ${email}`);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening to port ${process.env.PORT}`);
+});
